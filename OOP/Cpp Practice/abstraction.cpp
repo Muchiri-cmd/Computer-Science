c266752241788a150a3de66a5ed509ffcfa45215
Muchiri-cmd <davismuchiri21@gmail.com>
2023-11-19 13:41:59 +0300

    Add abstraction practice

---
+++ OOP/Cpp Practice/abstraction.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Smartphone{
+public:
+    virtual void TakeASelfie()=0;
+    virtual void MakeACall()=0;
+    //whoever inherits this class needs to implelement the selfie functionality
+};
+class Android:public Smartphone{
+public:
+    void TakeASelfie(){
+        cout << "Android Selfie\n" <<endl;
+    }
+    void MakeACall(){
+        cout << "Android call\n" << endl;
+    }
+
+};
+class Iphone:public Smartphone{
+public:
+    void TakeASelfie(){
+        cout << "Iphone selfie\n" <<endl;
+    }
+    void MakeACall(){
+        cout << "Iphone call\n" << endl;
+    }
+};
+
+int main(){
+
+    Smartphone * s1 = new Iphone();//base class pointer to an object of derived class
+    s1->TakeASelfie();
+    s1->MakeACall();
+
+    Smartphone * s2 = new Android();
+    s2->TakeASelfie();
+    s2->MakeACall();
+
+}
